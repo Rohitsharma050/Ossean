19cdca9b9e76afbfcfd5f4d6a25f7f749edf4f21
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-12-24 00:16:32 +0530

    Change EMAILJS user_id to use PRIVATE_KEY

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -17,7 +17,7 @@ const sendEmail = async (templateParams) => {
       body: JSON.stringify({
         service_id: process.env.EMAILJS_SERVICE_ID,
         template_id: process.env.EMAILJS_TEMPLATE_ID,
-        user_id: process.env.EMAILJS_PUBLIC_KEY,
+        user_id: process.env.EMAILJS_PRIVATE_KEY,
         template_params: templateParams,
       }),
     }
